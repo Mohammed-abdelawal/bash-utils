0c7dc55996c24fd20670a58c96c8394a46211fd0
Mohammed Abdelawal <mohammedabdelawaldeveloper@gmail.com>
2024-02-18 22:47:23 +0200

    Update setup_ubuntu.sh
    add docker compose for apt

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -65,7 +65,7 @@ if ! is_package_installed docker-ce; then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     apt-cache policy docker-ce
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin | tee -a "$log_file"
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin  docker-compose | tee -a "$log_file"
 else
     log "Docker is already installed"
 fi
